290445e1e0ebf70799a5c96fa13a479b0c7bb879
Gaitan Javier Emanuel <javii.gaitan@gmail.com>
2024-08-06 10:58:13 -0300

    add (nosotros)

--- src/Pages/Nosotras/Nosotras.jsx
+++ src/Pages/Nosotras/Nosotras.jsx
@@ -1,68 +1,111 @@
 import React from 'react';
-import FOTO from '../../assets/Img/Marruecos.jpg';
 import Navbar from '../../Componentes/Navbar/Navbar';
 import Footer from '../../Componentes/Footer/Footer';
-
+import LINEAPUNTEADA from '../../assets/Nosotros/lineapunteada.png';
+import IMG1 from '../../assets/Nosotros/nosotros1.png';
+import IMG2 from '../../assets/Nosotros/nosotros2.png';
+import IMG3 from '../../assets/Nosotros/nosotros3.png';
+import IMG4 from '../../assets/Nosotros/nosotros4.png';
 
 const Nosotras = () => {
-    return (
-        <>
+  return (
+    <>
+      <Navbar />
+      <div className="relative min-h-screen">
+        <img
+          src={LINEAPUNTEADA}
+          alt="Línea punteada"
+          className="absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2 z-0"
+          style={{ height: '94%', width: '97%' }}
+        />
+        <div className="py-20 2xl:px-0 lg:px-12 px-4 2xl:mx-auto 2xl:container relative z-10">
+          <div className="md:flex items-center justify-between w-full">
+            <div className="w-full text-left mb-8">
+              <h1 className="text-4xl b-10 pb-10 font-bold">Sobre nosotros</h1>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row items-center w-full p-6 space-y-8 md:space-y-0 md:space-x-8 relative z-10">
+            <div className="w-full md:w-1/2 text-center md:text-left mb-4 md:mb-0" >
+              <p className="text-lg text-primaryVio" >Somos una agencia de turismo educativo de origen latino pensada para <strong>latinos</strong> con más de <strong>7 años</strong> de experiencia.</p>
+            </div>
+            <div className="w-full md:w-1/2">
+              <img
+                src={IMG1}
+                alt="Nosotros"
+                className="w-full h-auto rounded-lg"
+              />
+            </div>
+          </div>
 
-            <Navbar />
-            <div>
+          <div className="flex flex-col md:flex-row items-center w-full p-6 space-y-8 md:space-y-0 md:space-x-8 relative z-10">
+            <div className="w-full md:w-1/2">
+              <img
+                src={IMG2}
+                alt="Nosotros"
+                className="w-full h-auto rounded-lg"
+              />
+            </div>
+            <div className="w-full md:w-1/2 text-center md:text-left mb-2 md:mb-0 p-2" style={{
+    paddingRight: '40px',
+    borderRightWidth: '40px'
+  }}>
+  <p className="text-lg text-primaryVio">Nuestra sede central está en Dublín, Irlanda y tenemos más de <strong>20 representantes</strong> de diferentes nacionalidades para ayudarte en distintas partes del mundo.</p>
+</div>
+          </div>
 
-                <div className="py-20 2xl:px-0 lg:px-12 px-4 2xl:mx-auto 2xl:container bg-gray-50" style={{ height: 1000 }}>
-                    <div className="md:flex items-center justify-between w-full bg-gray-50">
-                        <div className="xl:w-1/3 md:w-1/2 w-full">
-                            <h1 role="heading" className="focus:outline-none md:w-60 font- md:text-6xl text-4xl leading-tight text-primaryVio">
-                                De corazón viajero y aventurero
+          <div className="flex flex-col md:flex-row items-center w-full p-6 space-y-8 md:space-y-0 md:space-x-8 relative z-10">
+            <div className="w-full md:w-1/2 text-center md:text-left mb-4 md:mb-0">
+              <p className="text-lg text-primaryVio">Nuestra comunidad ya suma <strong>+2.500 travellers</strong> quienes nos eligieron para vivir esta experiencia y crecer profesionalmente.</p>
+            </div>
+            <div className="w-full md:w-1/2">
+              <img
+                src={IMG3}
+                alt="Nosotros"
+                className="w-full h-auto rounded-lg"
+              />
+            </div>
+          </div>
 
-                            </h1>
+          <div className="flex flex-col md:flex-row items-center w-full p-6 space-y-8 md:space-y-0 md:space-x-8 relative z-10">
+            <div className="w-full md:w-1/2">
+              <img
+                src={IMG4}
+                alt="Nosotros"
+                className="w-full h-auto rounded-lg"
+              />
+            </div>
+            <div className="w-full md:w-1/2 text-center md:text-left mb-4 md:mb-0">
+              <p className="text-lg text-primaryVio">Vamos más allá de solo recomendarte una escuela, vivimos la experiencia y sabemos qué necesitas para que tu travesía en el extranjero sea exitosa.</p>
+            </div>
+          </div>
 
-                            <div className="mt-10">
-                                <p className="flex items-center md:p-3 p-1 focus:outline-none text-l font- leading-5 text-colorText focus:ring-2 focus:ring-offset-2 focus:ring-gray-700">
-                                    Quienes formamos Vaga-Mundo vivimos la experiencia de emigrar, viajar y capacitarnos en el exterior, para ahora poner a tu alcance todo nuestro conocimiento.
-
-                                    Somos la agencia de intercambios que te acompañará desde ahora a cumplir tu sueño de viajar, estudiar y trabajar en el exterior.
-
-                                    Desde nuestra sede en Irlanda y con representantes en distintos países, asesoramos gratuitamente a personas de todo el mundo 24/7 con más de 7 años de experiencia.
-
-                                    ¿Estás liste?
-                                </p>
-
-                                <div className="inline-block py-5">
-                                    <button className="elementor-button bg-primaryVio text-white rounded-full text-base font-medium py-1 px-6 transition-all duration-300">
-                                        Solicita tu cotización
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="xl:w-4/6 md:w-1/2 w-full xl:pl-48">
-
-                            <div className="w-full">
-                                <img src={FOTO} alt="Equipo de VM en marruecos" className="md:w-full sm:w-1/2 w-full rounded-lg " />
-
-                            </div>
-                        </div>
-                    </div>
-
-                 
-
-
-                        <Footer />
-
-                    </div>
-
-
+          <div className="relative z-10">
+            <h1 className="lg:text-5xl md:text-4xl text-2xl font-semibold leading-10 text-primaryVio mt-6 text-center">
+              Conoce a nuestra Comunidad
+            </h1>
+            <div className="flex justify-center mt-8">
+              <div className="w-full md:w-2/3 lg:w-1/2">
+                <div className="relative overflow-hidden pb-[56.25%] h-0">
+                  <iframe
+                    className="absolute top-0 left-0 w-full h-full"
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/KEfaVKVG9OU?si=VbyUGB1ZRBGaExWv"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                  ></iframe>
                 </div>
-
-
-
-
-
-        </>
-
-    )
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default Nosotras
+export default Nosotras;
